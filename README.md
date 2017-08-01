0fa8a4a67955c3b7814f38e90df3cca4752edc54
Graham Ollis <plicease@cpan.org>
2017-08-01 13:38:52 -0400

    initial implementation

---
+++ README.md
@@ -0,0 +1,49 @@
+# Alien::Role::Alt [![Build Status](https://secure.travis-ci.org/plicease/Alien-Role-Alt.png)](http://travis-ci.org/plicease/Alien-Role-Alt)
+
+Alien::Base role that supports alternates
+
+# SYNOPSIS
+
+From your [alienfile](https://metacpan.org/pod/alienfile)
+
+    use alienfile;
+    
+    plugin 'PkgConfig' => (
+      pkg_name => [ 'libfoo', 'libbar', ],
+    );
+
+The in your base class:
+
+    package Alien::Libfoo;
+    
+    use base qw( Alien::Base );
+    use Role::Tiny::With qw( with );
+    
+    with 'Alien::Role::Alt';
+    
+    1;
+
+# DESCRIPTION
+
+Some packages come with multiple libraries, and multiple `.pc` files to
+use with them.  This [Role::Tiny](https://metacpan.org/pod/Role::Tiny) role can be used with [Alien::Base](https://metacpan.org/pod/Alien::Base)
+to access different configurations.
+
+# METHODS
+
+## alt
+
+    my $new_alien = $old_alien->alt($alt_name);
+
+Returns an [Alien::Base](https://metacpan.org/pod/Alien::Base) instance with the alternate configuration.
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Alien/Role/Alt.pm
+++ lib/Alien/Role/Alt.pm
@@ -3,8 +3,97 @@ package Alien::Role::Alt;
 use strict;
 use warnings;
 use 5.008001;
+use Role::Tiny;
+use Storable ();
+use Carp ();
 
 # ABSTRACT: Alien::Base role that supports alternates
 # VERSION
+
+=head1 SYNOPSIS
+
+From your L<alienfile>
+
+ use alienfile;
+ 
+ plugin 'PkgConfig' => (
+   pkg_name => [ 'libfoo', 'libbar', ],
+ );
+
+The in your base class:
+
+ package Alien::Libfoo;
+ 
+ use base qw( Alien::Base );
+ use Role::Tiny::With qw( with );
+ 
+ with 'Alien::Role::Alt';
+ 
+ 1;
+
+=head1 DESCRIPTION
+
+Some packages come with multiple libraries, and multiple C<.pc> files to
+use with them.  This L<Role::Tiny> role can be used with L<Alien::Base>
+to access different configurations.
+
+=head1 METHODS
+
+=head2 alt
+
+ my $new_alien = $old_alien->alt($alt_name);
+
+Returns an L<Alien::Base> instance with the alternate configuration.
+
+=cut
+
+sub alt
+{
+  my($old, $name) = @_;
+  my $new = ref $old ? (ref $old)->new : $old->new;
+
+  my $orig;
+  
+  if(ref($old) && defined $old->{_alt})
+  { $orig = $old->{_alt}->{orig} }
+  else
+  { $orig = $old->runtime_prop }
+  
+  my $runtime_prop = Storable::dclone($orig);
+  
+  if($runtime_prop->{alt}->{$name})
+  {
+    foreach my $key (keys %{ $runtime_prop->{alt}->{$name} })
+    {
+      $runtime_prop->{$key} = $runtime_prop->{alt}->{$name}->{$key};
+    }
+  }
+  else
+  {
+    Carp::croak("no such alt: $name");
+  }
+
+  $new->{_alt} = {
+    runtime_prop => $runtime_prop,
+    orig         => $orig,
+  };   
+  
+  $new;
+}
+
+around runtime_prop => sub {
+  my $orig = shift;
+
+  my($self) = @_;
+  
+  if(ref($self) && defined $self->{_alt})
+  {
+    return $self->{_alt}->{runtime_prop};
+  }
+  else
+  {
+    return $orig->($self);
+  }
+};
 
 1;
